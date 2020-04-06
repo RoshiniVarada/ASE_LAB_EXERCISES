faa0260a76d9a1dc83c96b44aaeeece8dc249e26
RoshiniVarada <60275582+RoshiniVarada@users.noreply.github.com>
2020-04-05 21:17:09 -0500

    JWT
    JWT

---
+++ Tutorial9/LAB-9_ASE/JWTAuthentication/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  User_Infor:any
+  UserInfo: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+  getDetails() {
+    const token = localStorage.getItem('authorization');
+    const headers = new HttpHeaders().set('authorization', token);
+    this.http.get('http://localhost:8888/api/getDetails', {headers} ).subscribe( data => {
+      if(data) {
+        this.UserInfo = data.user;
+         this.User_Infor=true;
+      }
+      console.log(data);
+    });
+  }
+}
